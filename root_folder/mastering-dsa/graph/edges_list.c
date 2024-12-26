ce086ba8e941e49cc57eb823b737ee5f283f2725
PierceLe <haichau.hvsg@gmail.com>
2024-12-26 23:32:04 +0800

    refactor: segmentation error

--- root_folder/mastering-dsa/graph/edges_list.c
+++ root_folder/mastering-dsa/graph/edges_list.c
@@ -6,35 +6,35 @@ int main() {
     printf("Enter number of vertices and edges: \n");
     scanf("%d %d", &n_vertex, &n_edge);
 
-    int*** edges_list = (int***)malloc(n_vertex * sizeof(int**));
-    for (int i = 0; i < n_vertex; i++) {
-        edges_list[i] = (int**)malloc(sizeof(int*));
+    int*** edges_list = (int***)malloc((n_vertex + 1) * sizeof(int**));
+    for (int i = 0; i <= n_vertex; i++) {
+        edges_list[i] = (int**)malloc(0);
     }
-    for (int i = 0; i < n_edge; i++) {
+    for (int i = 0; i <= n_edge; i++) {
         printf("Please enter vertices u, v and its weight: ");
         int u, v, w;
         scanf("%d %d %d", &u, &v, &w);
+        edges_list[u] = (int**)realloc(edges_list[u],sizeof(edges_list[u]) * sizeof(int*) + sizeof(int*));
         edges_list[u][sizeof(edges_list[u]) / sizeof(int*) - 1] = (int*)malloc(2 * sizeof(int));
         edges_list[u][sizeof(edges_list[u]) / sizeof(int*) - 1][0] = v;
         edges_list[u][sizeof(edges_list[u]) / sizeof(int*) - 1][1] = w;
-        edges_list[u] = (int**)realloc(edges_list[u],sizeof(edges_list[u]) + sizeof(int*));
 
         edges_list[v][sizeof(edges_list[v]) / sizeof(int*) - 1] = (int*)malloc(2 * sizeof(int));
         edges_list[v][sizeof(edges_list[v]) / sizeof(int*) - 1][0] = u;
         edges_list[v][sizeof(edges_list[v]) / sizeof(int*) - 1][1] = w;
-        edges_list[v] = (int**)realloc(edges_list[v],sizeof(edges_list[u]) + sizeof(int*));
+        edges_list[v] = (int**)realloc(edges_list[v],sizeof(edges_list[u]) * sizeof(int*) + sizeof(int*));
     }
 
-    for (int i = 0; i < n_vertex; i++) {
+    for (int i = 1; i <= n_vertex; i++) {
         printf("Vertex %d: ", i);
         for (int j = 0; j < sizeof(edges_list[i]) / sizeof(int*); j++) {
-            printf("[ %d, %d ] ,", edges_list[i][j][0], edges_list[i][j][1]);
+            printf("[%d, %d], ", edges_list[i][j][0], edges_list[i][j][1]);
         }
         printf("\n");
     }
 
-    for (int i = 0; i < n_vertex; i++) {
-        for (int j = 0; j < sizeof(edges_list[i]) / sizeof(int*); j++) {
+    for (int i = 1; i <= n_vertex; i++) {
+        for (int j = 0; j < sizeof(edges_list[i]) / sizeof(int*) - 1; j++) {
             free(edges_list[i][j]);
         }
         free(edges_list[i]);
